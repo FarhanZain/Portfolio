2d679c69d7a03e2c2f4783ab1f6849275602d097
FarhanZain <farhanabdurrahmanzain@gmail.com>
2023-06-22 22:22:36 +0700

    add 2 figma link

--- src/pages/DataUI.jsx
+++ src/pages/DataUI.jsx
@@ -7,7 +7,8 @@ const DataUI = [
     judul: "Massive Project Lawan.id",
     deskripsi:
       "Project Studi Independen dengan nama aplikasi Lawan.id, merupakan aplikasi pelaporan kekerasan yang ada di kota Batam, project ini dibagi menjadi 2 platform yaitu mobile dan website, di project ini adalah tampilan dari UI websitenya. Project ini dikerjakan oleh kelompok Midnight Society dengan anggota Joy, Fitroh, Nurul, Naufal, Farhan, Aulia, Delima, Ila, dan Dian.",
-    figma: "",
+    figma:
+      "https://www.figma.com/file/FgwiPOl7RLiPKHtaIEPqsG/WEBSITE?type=design&node-id=1%3A4&mode=design&t=t6DqjbrCCUcHmnzi-1",
   },
   {
     id: "ui2",
@@ -23,7 +24,8 @@ const DataUI = [
     judul: "Micro Project Belajar Mengajar",
     deskripsi:
       "Project Studi Independen dengan nama aplikasi Belajar Mengajar, merupakan aplikasi yang membantu para tenaga pengajar di Indonesia dalam meningkatkan kompetensi mengajar, project ini dikerjakan oleh kelompok Great Mates dengan anggota Joy, Fitroh, Nurul, Naufal dan Farhan.",
-    figma: "",
+    figma:
+      "https://www.figma.com/file/KEVKk5TKYuv1d3rsu3kjcK/HiFi?type=design&node-id=24%3A3&mode=design&t=BRs7B6MyNQJ6J1C6-1",
   },
   {
     id: "ui4",
